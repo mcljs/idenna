231589e7ae7912a99a590286464ea8e5907f02b3
Michael Chacon <mcljs15@gmail.com>
2021-09-17 23:28:06 -0400

    add cockie

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,6 +16,7 @@ import Header from './Headers/Header'
 import {Footer2} from './Footer/Footer.tsx';
 import './layout.css'
 import Banner from '../images/banner.png'
+import CookieConsent from 'react-cookie-consent'
 const Layout = ({ children }) => {
 
 useDarkMode()
@@ -33,7 +34,36 @@ useDarkMode()
         <main className="dark:bg-[#121f3d]">{children}</main>
       <ScrollToTop />
       <Footer2/>
-     
+      <CookieConsent
+        buttonClasses="m-0 flex items-center justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-secondary hover:bg-secondary-400"
+        buttonText="Ok"
+        buttonWrapperClasses="flex space-x-2 items-center"
+        containerClasses="bg-white flex items-center space-x-2 p-2 sm:p-3"
+        contentClasses="m-0"
+        cookieName="cookie_consent"
+        cookieSecurity
+        declineButtonClasses="m-0 flex items-center justify-center px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-red-500 hover:bg-red-400"
+        declineButtonText="No"
+        enableDeclineButton
+        expires={365}
+        location="bottom"
+        onAccept={acceptConsent}
+        onDecline={declineConsent}
+        sameSite="strict"
+      >
+        <p className="ml-3 text-sm font-medium text-black">
+          Este sitio utiliza cookies para brindarle una mejor experiencia de usuario.
+          Para obtener más información, consulte nuestra{' '}
+          <a
+            className="underline"
+            href="https://www.privacypolicies.com/live/0468436e-4dd5-4c21-b8a2-0c42b47f695d"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Política de cookies
+          </a>
+        </p>
+      </CookieConsent>
 
    
     </>
